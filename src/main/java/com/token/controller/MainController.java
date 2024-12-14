db77d5c5188cf6b609804151c7ddd240d3ced8e1
小气大方的阿俊 <130641570+851543@users.noreply.github.com>
2024-12-14 18:07:41 +0800

    更新额外语音提示功能

--- src/main/java/com/token/controller/MainController.java
+++ src/main/java/com/token/controller/MainController.java
@@ -29,9 +29,15 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.shape.Circle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.controlsfx.control.PopOver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
@@ -43,6 +49,7 @@ import java.util.ResourceBundle;
 
 /**
  * 主页控制器类
+ *
  * @author 阿俊
  * @description
  */
@@ -74,7 +81,13 @@ public class MainController implements Initializable {
     private Button student;
 
     @FXML
-    private ToggleGroup  changeStatusGroup;
+    private ToggleGroup changeStatusGroup;
+
+    @FXML
+    private Circle cStatus;
+
+    @FXML
+    private Label status;
 
     private Parent popContent;
 
@@ -82,46 +95,59 @@ public class MainController implements Initializable {
 
     public static final PopOver popConfig = new PopOver();
 
+    private MediaPlayer mediaPlayer;
+
+    private static final Logger log = LoggerFactory.getLogger(ServiceHomeUtils.class);
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         loadContentPopup();
         showWeb();
-        if (!ObjectUtils.isEmpty(ServiceHomeUtils.getLoginUserInfo())){
-           User loginUserInfo = ServiceHomeUtils.getLoginUserInfo();
-           nameLabel.setText(loginUserInfo.getNickName());
-           String imagePath =  StringUtils.isEmpty(loginUserInfo.getAvatar()) ? ServiceHomeUtils.defaultImagePath : loginUserInfo.getAvatar();
-           Image image = new Image(new File(imagePath).toURI().toString());
-           Platform.runLater(()->{
-               avatar.setImage(image);
-           });
+        if (!ObjectUtils.isEmpty(ServiceHomeUtils.getLoginUserInfo())) {
+            User loginUserInfo = ServiceHomeUtils.getLoginUserInfo();
+            nameLabel.setText(loginUserInfo.getNickName());
+            String imagePath = StringUtils.isEmpty(loginUserInfo.getAvatar()) ? ServiceHomeUtils.defaultImagePath : loginUserInfo.getAvatar();
+            Image image = new Image(new File(imagePath).toURI().toString());
+            Platform.runLater(() -> {
+                avatar.setImage(image);
+            });
 
-            UserConfig userConfig = SpringUtils.getBean(UserConfigService.class).select(loginUserInfo.getId());
-            if (ObjectUtils.isEmpty(userConfig)){
-                userConfig = new UserConfig();
-                userConfig.setUserId(Math.toIntExact(loginUserInfo.getId()));
-                userConfig.setConfigKey(UserConfigConstant.USER_LOGIN_STATUS_KEY);
-                userConfig.setConfigName(UserConfigConstant.USER_LOGIN_STATUS_NAME);
-                userConfig.setConfigValue(LoginStatus.ONLINE.getCode());
-                SpringUtils.getBean(UserConfigService.class).insert(userConfig);
-                userConfig = SpringUtils.getBean(UserConfigService.class).select(loginUserInfo.getId());
-            }
-            if (userConfig.getConfigKey().equals(UserConfigConstant.USER_LOGIN_STATUS_NAME)){
-                if (userConfig.getConfigValue().equals(LoginStatus.ONLINE.getValue())){
-                    System.out.println("在线语音");
+            new Thread(() -> {
+                UserConfig userConfig = SpringUtils.getBean(UserConfigService.class).select(loginUserInfo.getId());
+                if (ObjectUtils.isEmpty(userConfig)) {
+                    userConfig = new UserConfig();
+                    userConfig.setUserId(Math.toIntExact(loginUserInfo.getId()));
+                    userConfig.setConfigKey(UserConfigConstant.USER_LOGIN_STATUS_KEY);
+                    userConfig.setConfigName(UserConfigConstant.USER_LOGIN_STATUS_NAME);
+                    userConfig.setConfigValue(LoginStatus.ONLINE.getCode());
+                    SpringUtils.getBean(UserConfigService.class).insert(userConfig);
+                    userConfig = SpringUtils.getBean(UserConfigService.class).select(loginUserInfo.getId());
                 }
-                if (userConfig.getConfigValue().equals(LoginStatus.LEAVE.getValue())){
-                    System.out.println("离开语音");
+                if (userConfig.getConfigKey().equals(UserConfigConstant.USER_LOGIN_STATUS_KEY)) {
+                    audioStart("/mp3/service_home.mp3");
+                    String configValue = userConfig.getConfigValue();
+                    Platform.runLater(() ->{
+                        changeStatusGroup.getToggles().stream()
+                                .filter(toggle -> toggle instanceof RadioButton)
+                                .forEach(toggle -> {
+                                    RadioButton radioButton = (RadioButton) toggle;
+                                    if (ServiceHomeUtils.setUserLoginStatus(radioButton.getText()).equals(configValue)) {
+                                        radioButton.setSelected(true);
+                                        cStatus.setFill(radioButton.getTextFill());
+                                        status.setText(radioButton.getText());
+                                    }
+                                });
+                    });
                 }
-                if (userConfig.getConfigValue().equals(LoginStatus.BUSY.getValue())){
-                    System.out.println("忙碌语音");
-                }
-            }
-       }
+            }).start();
+        }
         changeStatusGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
             @Override
             public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
                 if (newValue != null && newValue instanceof RadioButton) {
                     RadioButton selectedButton = (RadioButton) newValue;
+                    cStatus.setFill(selectedButton.getTextFill());
+                    status.setText(selectedButton.getText());
                     UserConfig userConfig = new UserConfig();
                     userConfig.setUserId(Math.toIntExact(ServiceHomeUtils.getLoginUserInfo().getId()));
                     userConfig.setConfigKey(UserConfigConstant.USER_LOGIN_STATUS_KEY);
@@ -134,7 +160,7 @@ public class MainController implements Initializable {
 
     @FXML
     private void userInfo(MouseEvent e) throws IOException {
-        if(e.getClickCount() == 2){
+        if (e.getClickCount() == 2) {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(App.class.getResource("/template/user/user_info.fxml"));
             StackPane target = loader.load();
@@ -166,19 +192,19 @@ public class MainController implements Initializable {
         }
     }
 
-    private void showWeb(){
-        if (ObjectUtils.isEmpty(ServiceHomeUtils.getLoginUserRole())){
+    private void showWeb() {
+        if (ObjectUtils.isEmpty(ServiceHomeUtils.getLoginUserRole())) {
             return;
         }
-        if (ServiceHomeUtils.getLoginUserRole() == UserRole.ADMIN){
-            SpringUtils.getBean(StageManager.class).switchContent(FxmlView.REPAIR,body);
+        if (ServiceHomeUtils.getLoginUserRole() == UserRole.ADMIN) {
+            SpringUtils.getBean(StageManager.class).switchContent(FxmlView.REPAIR, body);
             parent = dispatch.getParent();
             if (parent instanceof Pane) {
                 ((Pane) parent).getChildren().remove(dispatch);
             }
         }
-        if (ServiceHomeUtils.getLoginUserRole() == UserRole.STUDENT){
-            SpringUtils.getBean(StageManager.class).switchContent(FxmlView.REPAIR,body);
+        if (ServiceHomeUtils.getLoginUserRole() == UserRole.STUDENT) {
+            SpringUtils.getBean(StageManager.class).switchContent(FxmlView.REPAIR, body);
             parent = student.getParent();
             if (parent instanceof Pane) {
                 ((Pane) parent).getChildren().remove(student);
@@ -192,8 +218,8 @@ public class MainController implements Initializable {
                 ((Pane) parent).getChildren().remove(dispatch);
             }
         }
-        if (ServiceHomeUtils.getLoginUserRole() == UserRole.SERVICE){
-            SpringUtils.getBean(StageManager.class).switchContent(FxmlView.DISPATCH,body);
+        if (ServiceHomeUtils.getLoginUserRole() == UserRole.SERVICE) {
+            SpringUtils.getBean(StageManager.class).switchContent(FxmlView.DISPATCH, body);
             parent = student.getParent();
             if (parent instanceof Pane) {
                 ((Pane) parent).getChildren().remove(student);
@@ -234,19 +260,56 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    private void repairClicked(){
-        SpringUtils.getBean(StageManager.class).switchContent(FxmlView.REPAIR,body);
+    private void repairClicked() {
+        SpringUtils.getBean(StageManager.class).switchContent(FxmlView.REPAIR, body);
     }
 
     @FXML
-    private void serviceClicked(){
+    private void serviceClicked() {
         ServiceHomeUtils.setChangeRole(UserRole.SERVICE);
-        SpringUtils.getBean(StageManager.class).switchContent(FxmlView.USER,body);
+        SpringUtils.getBean(StageManager.class).switchContent(FxmlView.USER, body);
     }
 
     @FXML
-    private void studentClicked(){
+    private void studentClicked() {
         ServiceHomeUtils.setChangeRole(UserRole.STUDENT);
-        SpringUtils.getBean(StageManager.class).switchContent(FxmlView.USER,body);
+        SpringUtils.getBean(StageManager.class).switchContent(FxmlView.USER, body);
+    }
+
+    private void audioStart(String audioFile) {
+        Platform.runLater(() -> {
+            try {
+                // 创建Media对象
+                Media media = new Media(getClass().getResource(audioFile).toExternalForm());
+
+                // 创建MediaPlayer对象
+                mediaPlayer = new MediaPlayer(media);
+
+                // 添加错误监听器
+                mediaPlayer.errorProperty().addListener((observable, oldValue, newValue) -> {
+                    log.error("Error occurred while playing audio: " + newValue);
+                });
+
+                // 监听音乐播放结束
+                mediaPlayer.setOnEndOfMedia(() -> {
+                    log.info("Audio has finished playing");
+                    // 停止播放器
+                    mediaPlayer.stop();
+                });
+
+                // 开始播放音乐
+                mediaPlayer.play();
+
+                // 监听当前时间变化
+                mediaPlayer.currentTimeProperty().addListener((obs, oldTime, newTime) -> {
+                    if (newTime != null) {
+                        log.info("Current time: " + newTime);
+                    }
+                });
+
+            } catch (Exception e) {
+                log.error("Error playing audio: " + e.getMessage());
+            }
+        });
     }
 }
